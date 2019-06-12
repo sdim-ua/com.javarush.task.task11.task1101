a09d67266387524b621a77a09bf5ad8e8c7c78bf
sdim-ua <ingersol@yandex.ua>
2019-06-12 22:59:18 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,16 @@
+/* 
+Лошадь и Пегас
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+    }
+
+    public class Horse {
+
+    }
+
+    public class Pegasus extends Horse  {
+
+    }
+}
